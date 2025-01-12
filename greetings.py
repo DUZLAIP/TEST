af7f061858b330e7ed8055e9d9e08f8875685944
kirill <kirillromanov@inbox.ru>
2025-01-12 23:03:34 +0300

    grade 1. Task 1

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Kirill'
+title = 'home work'
+content = 'modul 1'
+status = 'in progress'
+create_date = 12,1,2025
+issue_date = 17,1,2025
+print(username)
+print(title )
+print(content)
+print(status)
+print(create_date)
+print(issue_date)
