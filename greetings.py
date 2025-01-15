43ea6cc8ec4f5648a340e22e9183ede5142622c1
DUZLAIP <kirillromanov@inbox.ru>
2025-01-16 00:40:09 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Имя пользователя: '
+title = 'Заголовок заметки: '
+content = 'Описание заметки: '
+status = 'Статус заметки: '
+created_data = 'Дата создания заметки д.м.г.: '
+issue_date = 'Дата истечения заметки д.м.г.: '
+print(username)
+print(title)
+print(content)
+print(status)
+print(created_data)
+print(issue_date)
